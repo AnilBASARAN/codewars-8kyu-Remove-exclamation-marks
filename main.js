05cec5d01e474f7db56fb1c47759b9ef75636feb
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2022-04-27 00:11:10 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+let removeExclamationMarks = (s) => s.split('').filter(x=> x != '!' ).join('')
+
+
+/*
+8kyu Remove exclamation marks
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
